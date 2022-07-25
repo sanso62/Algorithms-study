8224373f2adc42eda9ef868c8520d765187cafbb
sanso62 <jmd9762@gmail.com>
2022-07-25 18:10:54 +0900

    Leetcode_2, 24, 206

---
+++ mindong/Linked_List/Leetcode_206.py
@@ -0,0 +1,13 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        node=None
+        while head:
+            node, node.next, head= head, node, head.next
+        
+        return node
+        
