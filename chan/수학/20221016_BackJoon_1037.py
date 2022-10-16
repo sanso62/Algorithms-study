ea0f8e2a5ac530307bb058c79f8413e073371d65
CHANEE LEE <leecks1212@gmail.com>
2022-10-16 13:08:06 +0900

    1037

--- chan/수학/20221016_BackJoon_1037.py
+++ chan/수학/20221016_BackJoon_1037.py
@@ -1,1 +1,19 @@
-TEST
+cnt = int(input())
+num_arr = list(map(int, input().split()))
+
+num_arr.sort()
+
+for target_value in range(1,1000000):
+    find_value_flag = True
+    for num in num_arr:
+        if( target_value % num != 0 ):
+            find_value_flag = False
+            break
+
+    if( find_value_flag ):
+        if( target_value in num_arr ):
+            print(target_value * num_arr[0])
+            break
+        else:
+            print(target_value)
+            break
