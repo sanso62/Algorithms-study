4a4c4b36c4415860ef8c5101110cca95e7daa475
CHANEE LEE <leecks1212@gmail.com>
2022-10-23 13:04:16 +0900

    update 2309

--- chan/부르트포스/20221022_BackJoon_2309.py
+++ chan/부르트포스/20221022_BackJoon_2309.py
@@ -1,39 +1,39 @@
 # 2309번 - 일곱 난쟁이
 
-def find_seven_height_sum_100(cur_index, cur_dwarf_cnt, height_sum, cur_height):
-    visited[cur_index] = 1
-
-    if( height_sum +  cur_height <= 100 ):
-        height_sum = height_sum+ cur_height
-    else:
-        return height_sum
-
-    if (cur_dwarf_cnt == 7):
-        if (height_sum == 100):
-            return height_sum
-
-    for index in range (0,9):
-        # 방문안한 곳만 들리기
-        if (visited[index] == 0):
-            height_sum = find_seven_height_sum_100(index, cur_dwarf_cnt+1, height_sum, height_arr[index])
-            visited[index] = 0
-
-# Main 시작
 height_arr =[]
-visited =[0,0,0,0,0,0,0,0,0]
+total_height_sum = 0
 # 9명의 키 입력
 for i in range (1,10):
     height = int(input())
     height_arr.append(height)
-
-for index in range (0,9):
-    find_seven_height_sum_100(index,1,0,height_arr[index])
+    total_height_sum = total_height_sum + height
 
 #print(height_arr)
 
-# 9명 중에 7명 골라서 키의 합이 100인 경우 찾기
-# 모든 조합의 경우의 수는 9! 팩토리얼
+height_arr.sort()
 
+# 9명 중에 7명 골라서 키의 합이 100인 경우 찾기
+# 9명중 7명 고르는건 9명 중 2명 고르는 것과 같음
+stop_flag = False
+for i in range (0,9):
+    if(stop_flag):
+        break
+    for j in range (i,9):
+        if (i==j):
+            continue
+
+        temp_sum = total_height_sum - height_arr[i] - height_arr[j]
+        if(temp_sum == 100):
+            height_arr[i] = 0
+            height_arr[j] = 0
+            stop_flag = True
+
+        if (stop_flag):
+            break
+
+for height in height_arr:
+    if(height != 0):
+        print(height)
 
 
 
