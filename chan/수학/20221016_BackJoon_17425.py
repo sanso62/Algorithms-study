874ce5c995d63f38896fcdd46754fb34dbc53bd9
CHANEE LEE <leecks1212@gmail.com>
2022-10-23 14:06:41 +0900

    update code

--- chan/수학/20221016_BackJoon_17425.py
+++ chan/수학/20221016_BackJoon_17425.py
@@ -1,30 +1,18 @@
 # 17425번 - 약수의 합
 
-def f(num):
-    temp_sum = 0
-    for i in range(1, num+1):
-        if (num % i == 0):
-            temp_sum += i
-
-    return temp_sum
-
-
-def g(sum, num):
-    sum += num
-
-    return sum
-
-def print_result():
-    max_num = int(input())
-
-    sum = 0
-    for i in range(1,max_num+1):
-        num = f(i)
-        sum = g(sum, num)
-
-    print (sum)
-
-cnt = int (input())
-
-for i in range(0,cnt):
-    print_result()
+MAX = 1000000;
+d = [1]*(MAX+1)
+s = [0]*(MAX+1)
+for i in range(2, MAX+1):
+    j = 1
+    while i*j <= MAX:
+        d[i*j] += i
+        j += 1
+for i in range(1, MAX+1):
+    s[i] = s[i-1] + d[i]
+t = int(input())
+ans = []
+for _ in range(t):
+    n = int(input())
+    ans.append(s[n])
+print('\n'.join(map(str,ans))+'\n')
--- chan/수학/20221016_BackJoon_17427.py
+++ chan/수학/20221016_BackJoon_17427.py
@@ -1,25 +1,9 @@
 # 17427번 - 약수의 합 2
 
-def f(num):
-    temp_sum = 0
-    for i in range(1, num+1):
-        if (num % i == 0):
-            temp_sum += i
+n = int(input())
+ans = 0
+for i in range(1, n+1):
+    ans += (n//i)*i
+print(ans)
 
-    return temp_sum
-
-
-def g(sum, num):
-    sum += num
-
-    return sum
-
-max_num = int(input())
-
-sum = 0
-for i in range(1,max_num+1):
-    num = f(i)
-    sum = g(sum, num)
-
-print (sum)
 
