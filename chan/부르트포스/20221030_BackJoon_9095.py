d142e09b71b29da451d32d864a0ff92c7b85ac0c
CHANEE LEE <leecks1212@gmail.com>
2022-11-20 12:52:11 +0900

    aaa

--- chan/부르트포스/20221030_BackJoon_9095.py
+++ chan/부르트포스/20221030_BackJoon_9095.py
@@ -1,1 +1,12 @@
 # 9095번 - 1,2,3 더하기
+
+dp = [0,1,2,4,0,0,0,0,0,0,0]
+
+for i in range(4,11):
+    dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+
+n = int(input())
+
+for _ in range(0,n):
+    num = int(input())
+    print(dp[num])
---
+++ chan/부르트포스/20221120_BackJoon_1759.py
@@ -0,0 +1,11 @@
+# 1759번 - 암호 만들기
+
+L, C = map(input().split(), int)
+
+# L 선택 몇개
+# C 알파벳 몇개
+
+alpha_input = input().split()
+alpha_arr = list(alpha_input)
+alpha_arr.sort()
+
